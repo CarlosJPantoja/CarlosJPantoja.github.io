a405fe9275d2b2d76b6ff82069e5d27d1a2e01ef
CarlosJPantoja <carlosjpantoja@outlook.com>
2023-02-28 16:18:20 -0500

    Curriculum

---
+++ js/scripts.js
@@ -0,0 +1,53 @@
+window.addEventListener('DOMContentLoaded', event => {
+
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+            document.body.querySelector('.progress-container').style.visibility = "hidden";
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+            document.body.querySelector('.progress-container').style.visibility = "visible";
+        }
+
+    };
+
+    navbarShrink();
+
+    document.addEventListener('scroll', navbarShrink);
+
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 72,
+        });
+    };
+
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
